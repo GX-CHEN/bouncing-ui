817dbe481d6c62fbf0ba85e96b09c3cc4d585c31
Gongxia Chen <chengongxia1990@gmail.com>
2020-01-14 22:15:43 -0500

    add plan for demon slayer implementation

---
+++ demon_slayer(鬼灭之刃)/thunder_breathing.js
@@ -0,0 +1,2 @@
+// Implementation of thunder breathing technique (雷之呼吸全集中 - 霹雳一闪)
+// Listen to mouse event, then show animation based on hover/click/moving
---
+++ demon_slayer(鬼灭之刃)/water_breathing.js
@@ -0,0 +1,2 @@
+// Implementation of water breathing technique (水之呼吸全集中)
+// Listen to mouse event, then show water breath animation based on hover/click/moving
